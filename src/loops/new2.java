848817bd1a0de1801e8b168428f071f7f76692fd
Engharshita <harshitapal5678@gmail.com>
2026-01-21 12:24:13 +0530

    AddedNew

---
+++ src/loops/new2.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class new2 {
+    public static void main(String args[]){
+        for(int i=0;i<10;i++){
+            System.out.println("Hello world");
+
+    }
+
+
+    }
+}
